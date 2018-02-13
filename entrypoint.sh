31f242a40bbf9d2261168429137485b54f231278
CalebC <calebc@indeed.com>
2018-02-13 13:47:36 -0600

    Update private key secret name.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cp /run/secrets/wes_priv_key ${HOME}/.ssh/id_rsa
+cp /run/secrets/wes-git-private-key ${HOME}/.ssh/id_rsa
 chmod 400 ${HOME}/.ssh/id_rsa
 
 supervisord --nodaemon
