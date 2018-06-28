aaee3fdd421c90faf0cacaada0f856de14ff509e
Caleb Coffie <CalebCoffie@gmail.com>
2018-06-28 16:46:42 -0500

    Incrementing version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 3:
 
 setup(
     name='wes',
-    version='2.0.2',
+    version='2.0.3',
     packages=find_packages(),
     author='Caleb Coffie',
     author_email='calebc@indeed.com',
